1e0e483dfe3428c997ef1a1cfc3cab4816d342b2
DELLPC1 <DELLPC1@DESKTOP-AA9KGR9>
2022-08-20 23:39:11 +0530

    Add project files.

---
+++ BootStrapPKRtravels/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BootStrapPKRtravels.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BootStrapPKRtravels.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            List<MstBus> qry = new List<MstBus>();
+            using (MYBUSEntities dc = new MYBUSEntities())
+            {
+                qry = (from s in dc.BusInfoes
+                       where s.BoardingPoint == "MUM"
+
+                       select new MstBus
+                       {
+                           BusID = s.BusID,
+                           BoardingPoint = s.BoardingPoint,
+                           TravelDate = s.TravelDate,
+                           Amount = s.Amount,
+                           Rating = s.Rating,
+
+
+                       }).ToList();
+
+            }
+            return View(qry);
+        }
+    }
+}
